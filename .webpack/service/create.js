49b48ab9b466d6fc47deee07bc3e6a6654cc43e2
tylernchls <tylernchls@gmail.com>
2018-03-31 19:13:40 -1000

    fixes insertion of note to db

--- .webpack/service/create.js
+++ .webpack/service/create.js
@@ -85,7 +85,31 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nvar _stringify = __webpack_require__(/*! babel-runtime/core-js/json/stringify */ \"babel-runtime/core-js/json/stringify\");\n\nvar _stringify2 = _interopRequireDefault(_stringify);\n\nexports.main = main;\n\nvar _uuid = __webpack_require__(/*! uuid */ \"uuid\");\n\nvar _uuid2 = _interopRequireDefault(_uuid);\n\nvar _awsSdk = __webpack_require__(/*! aws-sdk */ \"aws-sdk\");\n\nvar _awsSdk2 = _interopRequireDefault(_awsSdk);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n_awsSdk2.default.config.update({ region: \"us-west-2\" });\nvar dynamoDb = new _awsSdk2.default.DynamoDB.DocumentClient();\n\nfunction main(event, context, callback) {\n  var data = JSON.parse(event.body);\n\n  var params = {\n    TableName: \"notes\",\n    Item: {\n      userId: event.requestContext.identity.cognitoIdentityId,\n      noteId: _uuid2.default.v1(),\n      content: data.content,\n      attachment: data.attachment,\n      createdAt: new Date().getTime()\n    }\n  };\n\n  dynamoDb.put(params, function (error, data) {\n    console.log('data', params);\n\n    var headers = {\n      \"Access-Control-Allow-Origin\": \"*\",\n      \"Access-Control-Allow-Credentials\": true\n    };\n\n    if (error) {\n      var _response = {\n        statusCode: 500,\n        headers: headers,\n        body: (0, _stringify2.default)({ status: false })\n      };\n      callback(null, _response);\n      return;\n    }\n\n    var response = {\n      statusCode: 200,\n      headers: headers,\n      body: (0, _stringify2.default)(params.Item)\n    };\n    callback(null, response);\n  });\n}\n\n//# sourceURL=webpack:///./create.js?");
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexports.main = undefined;\n\nvar _regenerator = __webpack_require__(/*! babel-runtime/regenerator */ \"babel-runtime/regenerator\");\n\nvar _regenerator2 = _interopRequireDefault(_regenerator);\n\nvar _asyncToGenerator2 = __webpack_require__(/*! babel-runtime/helpers/asyncToGenerator */ \"babel-runtime/helpers/asyncToGenerator\");\n\nvar _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);\n\nvar main = exports.main = function () {\n  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(event, context, callback) {\n    var data, params;\n    return _regenerator2.default.wrap(function _callee$(_context) {\n      while (1) {\n        switch (_context.prev = _context.next) {\n          case 0:\n            data = JSON.parse(event.body);\n            params = {\n              TableName: \"notes\",\n              Item: {\n                userid: event.requestContext.identity.cognitoIdentityId,\n                noteid: _uuid2.default.v1(),\n                content: data.content,\n                attachment: data.attachment,\n                createdAt: new Date().getTime()\n              }\n            };\n\n\n            console.log('params', params);\n\n            _context.prev = 3;\n            _context.next = 6;\n            return dynamoDbLib.call(\"put\", params);\n\n          case 6:\n\n            callback(null, (0, _responseLib.success)(params.Item));\n            _context.next = 12;\n            break;\n\n          case 9:\n            _context.prev = 9;\n            _context.t0 = _context[\"catch\"](3);\n\n            callback(null, (0, _responseLib.failure)({ status: false }));\n\n          case 12:\n          case \"end\":\n            return _context.stop();\n        }\n      }\n    }, _callee, this, [[3, 9]]);\n  }));\n\n  return function main(_x, _x2, _x3) {\n    return _ref.apply(this, arguments);\n  };\n}();\n\nvar _uuid = __webpack_require__(/*! uuid */ \"uuid\");\n\nvar _uuid2 = _interopRequireDefault(_uuid);\n\nvar _dynamodbLib = __webpack_require__(/*! ./libs/dynamodb-lib */ \"./libs/dynamodb-lib.js\");\n\nvar dynamoDbLib = _interopRequireWildcard(_dynamodbLib);\n\nvar _responseLib = __webpack_require__(/*! ./libs/response-lib */ \"./libs/response-lib.js\");\n\nfunction _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n//# sourceURL=webpack:///./create.js?");
+
+/***/ }),
+
+/***/ "./libs/dynamodb-lib.js":
+/*!******************************!*\
+  !*** ./libs/dynamodb-lib.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexports.call = call;\n\nvar _awsSdk = __webpack_require__(/*! aws-sdk */ \"aws-sdk\");\n\nvar _awsSdk2 = _interopRequireDefault(_awsSdk);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\n_awsSdk2.default.config.update({ region: \"us-west-2\" });\n\nfunction call(action, params) {\n  var dynamoDb = new _awsSdk2.default.DynamoDB.DocumentClient();\n\n  return dynamoDb[action](params).promise();\n}\n\n//# sourceURL=webpack:///./libs/dynamodb-lib.js?");
+
+/***/ }),
+
+/***/ "./libs/response-lib.js":
+/*!******************************!*\
+  !*** ./libs/response-lib.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nvar _stringify = __webpack_require__(/*! babel-runtime/core-js/json/stringify */ \"babel-runtime/core-js/json/stringify\");\n\nvar _stringify2 = _interopRequireDefault(_stringify);\n\nexports.success = success;\nexports.failure = failure;\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\nfunction success(body) {\n  return buildResponse(200, body);\n}\n\nfunction failure(body) {\n  return buildResponse(500, body);\n}\n\nfunction buildResponse(statusCode, body) {\n  return {\n    statusCode: statusCode,\n    headers: {\n      \"Access-Control-Allow-Origin\": \"*\",\n      \"Access-Control-Allow-Credentials\": true\n    },\n    body: (0, _stringify2.default)(body)\n  };\n}\n\n//# sourceURL=webpack:///./libs/response-lib.js?");
 
 /***/ }),
 
@@ -108,6 +132,28 @@ eval("module.exports = require(\"aws-sdk\");\n\n//# sourceURL=webpack:///externa
 /***/ (function(module, exports) {
 
 eval("module.exports = require(\"babel-runtime/core-js/json/stringify\");\n\n//# sourceURL=webpack:///external_%22babel-runtime/core-js/json/stringify%22?");
+
+/***/ }),
+
+/***/ "babel-runtime/helpers/asyncToGenerator":
+/*!*********************************************************!*\
+  !*** external "babel-runtime/helpers/asyncToGenerator" ***!
+  \*********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"babel-runtime/helpers/asyncToGenerator\");\n\n//# sourceURL=webpack:///external_%22babel-runtime/helpers/asyncToGenerator%22?");
+
+/***/ }),
+
+/***/ "babel-runtime/regenerator":
+/*!********************************************!*\
+  !*** external "babel-runtime/regenerator" ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = require(\"babel-runtime/regenerator\");\n\n//# sourceURL=webpack:///external_%22babel-runtime/regenerator%22?");
 
 /***/ }),
 
--- create.js
+++ create.js
@@ -1,46 +1,29 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
-AWS.config.update({ region: "us-west-2" });
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback) {
+export async function main(event, context, callback) {
   const data = JSON.parse(event.body);
-
+  
   const params = {
     TableName: "notes",
     Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
+      userid: event.requestContext.identity.cognitoIdentityId,
+      noteid: uuid.v1(),
       content: data.content,
       attachment: data.attachment,
       createdAt: new Date().getTime()
     }
   };
 
-  dynamoDb.put(params, (error, data) => {
-  console.log('data', params)
+  console.log('params', params)
+  
+
+  try {
+    await dynamoDbLib.call("put", params);
     
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-      callback(null, response);
-      return;
-    }
-
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
+    callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
 }
